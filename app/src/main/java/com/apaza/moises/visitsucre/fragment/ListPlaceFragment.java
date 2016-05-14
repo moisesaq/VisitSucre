cfae525fc75ff2ed183fbb788c674de7ae4ed162
moisse <moisesapaza07@gmail.com>
2016-05-13 23:19:55 -0300

    list place

--- app/src/main/java/com/apaza/moises/visitsucre/fragment/ListPlaceFragment.java
+++ app/src/main/java/com/apaza/moises/visitsucre/fragment/ListPlaceFragment.java
@@ -105,8 +105,8 @@ public class ListPlaceFragment extends Fragment implements View.OnClickListener{
                 }
             });
             //requestQueue.add(jsonArrayRequest);
-            Global.getIntance(getActivity().getApplicationContext()).addToRequestQueue(jsonArrayRequest);
-            ImageLoader imageLoader = Global.getIntance(getActivity().getApplicationContext()).getImageLoader();
+            Global.getIntance(getActivity()).addToRequestQueue(jsonArrayRequest);
+            ImageLoader imageLoader = Global.getIntance(getActivity()).getImageLoader();
             imageLoader.get(urlImage, ImageLoader.getImageListener(imagePost, R.mipmap.ic_communication, R.mipmap.default_profile));
             imagePost.setImageUrl(urlImage, imageLoader);
         }catch (Exception e){
