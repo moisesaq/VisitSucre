f2d853941181407f0e1ca34e577112ab1a845c20
moisse <moisesapaza07@gmail.com>
2016-06-01 01:39:03 -0300

    implementing list places

--- app/src/main/java/com/apaza/moises/visitsucre/fragment/PlaceListFragment.java
+++ app/src/main/java/com/apaza/moises/visitsucre/fragment/PlaceListFragment.java
@@ -1,23 +1,34 @@
 package com.apaza.moises.visitsucre.fragment;
 
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.apaza.moises.visitsucre.R;
+import com.apaza.moises.visitsucre.fragment.adapter.PlaceAdapter;
+import com.apaza.moises.visitsucre.global.Utils;
+import com.apaza.moises.visitsucre.provider.ContractVisitSucre;
 
-public class PlaceListFragment extends Fragment implements View.OnClickListener{
+public class PlaceListFragment extends Fragment implements PlaceAdapter.OnPlaceItemClickListener, LoaderManager.LoaderCallbacks<Cursor>{
     private static final String ARG_PARAM1 = "param1";
     private String mParam1;
 
     private View view;
+    private RecyclerView listPlaces;
+    private LinearLayoutManager linearLayoutManager;
+    private PlaceAdapter adapter;
 
     private OnFragmentInteractionListener mListener;
-
-
 
     public static PlaceListFragment newInstance(String param1) {
         PlaceListFragment fragment = new PlaceListFragment();
@@ -47,19 +58,44 @@ public class PlaceListFragment extends Fragment implements View.OnClickListener{
     }
 
     private void setup(){
+        listPlaces = (RecyclerView)view.findViewById(R.id.listPlaces);
+        listPlaces.setHasFixedSize(true);
+
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        listPlaces.setLayoutManager(linearLayoutManager);
+
+        adapter = new PlaceAdapter(getActivity(), this);
+        listPlaces.setAdapter(adapter);
+
+        getLoaderManager().restartLoader(1, null, this);
     }
 
-
+    //This method is from adapter
     @Override
-    public void onClick(View view){
+    public void onPlaceClick(PlaceAdapter.ViewHolder viewHolder, String idPlace) {
+        Snackbar.make(getActivity().findViewById(android.R.id.content), String.format("ID place: %s", idPlace), Snackbar.LENGTH_SHORT).show();
     }
-
-
 
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(getActivity(), ContractVisitSucre.Place.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            if(adapter != null)
+                adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 
     @Override
--- app/src/main/java/com/apaza/moises/visitsucre/fragment/adapter/PlaceAdapter.java
+++ app/src/main/java/com/apaza/moises/visitsucre/fragment/adapter/PlaceAdapter.java
@@ -3,21 +3,26 @@ package com.apaza.moises.visitsucre.fragment.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by moises on 31/05/16.
- */
+import com.apaza.moises.visitsucre.R;
+import com.apaza.moises.visitsucre.provider.ContractVisitSucre;
+import com.bumptech.glide.Glide;
+
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder>{
 
     private final Context context;
     private Cursor cursor;
 
-    private OnPlaceItemClickListener listener;
+    public OnPlaceItemClickListener listener;
 
-    interface OnPlaceItemClickListener{
-        public void onPlaceClick(ViewHolder viewHolder, String idPlace);
+    public interface OnPlaceItemClickListener{
+        void onPlaceClick(ViewHolder viewHolder, String idPlace);
     }
 
     public PlaceAdapter(Context context, OnPlaceItemClickListener listener){
@@ -26,28 +31,86 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder>{
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_item, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        cursor.moveToPosition(position);
 
+        String text;
+        //text = cursor.getString(cursor.getColumnIndex(ContractVisitSucre.Place.PATH_IMAGE));
+        //Glide.with(context).load(text).centerCrop().into(holder.imagePlace);
+        text = cursor.getString(cursor.getColumnIndex(ContractVisitSucre.Place.NAME));
+        holder.namePlace.setText(text);
+        text = cursor.getString(cursor.getColumnIndex(ContractVisitSucre.Place.ID_CATEGORY));
+        holder.category.setText(text);
+        text = cursor.getString(cursor.getColumnIndex(ContractVisitSucre.Place.DESCRIPTION));
+        holder.description.setText(text);
     }
 
     @Override
     public int getItemCount() {
+        if(cursor != null)
+            return cursor.getCount();
         return 0;
+    }
+
+    public String getIdPlace(int position){
+        if(cursor != null){
+            if(cursor.moveToPosition(position))
+                return cursor.getString(cursor.getColumnIndex(ContractVisitSucre.Place.ID));
+            else
+                return "";
+        }else{
+            return "";
+        }
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        private ImageView imagePlace;
+        private TextView namePlace, category, description;
+        private Button share, explore;
+
         public ViewHolder(View v){
             super(v);
+            imagePlace = (ImageView)v.findViewById(R.id.imagePlace);
+            namePlace = (TextView)v.findViewById(R.id.namePlace);
+            category = (TextView)v.findViewById(R.id.category);
+            share = (Button)v.findViewById(R.id.share);
+            explore = (Button)v.findViewById(R.id.explore);
+            description = (TextView) v.findViewById(R.id.description);
+            share.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-
+            listener.onPlaceClick(this, getIdPlace(getAdapterPosition()));
         }
     }
+
+    public void swapCursor(Cursor newCursor){
+        if(newCursor != null){
+            cursor = newCursor;
+            notifyDataSetChanged();
+        }
+    }
+
+    public Cursor getCursor(){
+        return cursor;
+    }
+
+    /*this.idPlace = idPlace;
+    this.code = code;
+    this.name = name;
+    this.address = address;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.description = description;
+    this.pathImage = pathImage;
+    this.date = date;
+    this.idCategory = idCategory;*/
+
 }
